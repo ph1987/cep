d0e89ebeb34e7deab5010aebc640b37d194f1a43
philipe.t.censo <philipe.t.censo@CAN00545474>
2017-10-23 17:39:29 -0200

    consulta cep e retorna endereço

---
+++ cep.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#cep').blur(function(){
+        var cep = $('#cep').val() || '';
+        if(!cep){
+            return;
+        }
+        var url = 'http://viacep.com.br/ws/' + cep + '/json';
+        $.getJSON(url, function(data){
+            if("error" in data){
+                return;
+            }
+            $('#rua').val(data.logradouro);
+            $('#bairro').val(data.bairro);
+            $('#cidade').val(data.localidade);
+            $('#uf').val(data.uf);
+        });
+    });
+});
